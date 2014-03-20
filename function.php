4ed8f7d6dded025959b6e48acd4174daa360bf81
LarryStanley <Stanley@mcl46.mcl.math.ncu.edu.tw>
2014-03-20 15:14:13 +0800

    Fix the bug of label.

--- function.php
+++ function.php
@@ -1,7 +1,7 @@
 <?php
 	function printTextForm($id, $labelName, $value, $additionClass, $placeholder) {
 		$result = "<div class='form-group".$additionClass."' id='".$id."Form'>";
-		$result = $result."<label class='col-sm-4 control-label' for='".$id."'>".$idName."</label>";
+		$result = $result."<label class='col-sm-4 control-label'>".$labelName."</label>";
 		$result = $result."<div class='col-sm-8'><input type='text' class='form-control' id='".$id."' placeholder='".$placeholder."' value='".$value."'>";
 		$result = $result."</div></div>";
 		return $result;
