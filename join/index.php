8e581299b61db503438f45de6174b392f5baccae
LarryStanley <Stanley@mcl46.mcl.math.ncu.edu.tw>
2014-03-20 15:43:04 +0800

    Add button and processJoin.php.

--- join/index.php
+++ join/index.php
@@ -20,6 +20,7 @@
 </head>
 <body>
 <div class='container joinForm'>
+	<h1 align='center'>第15屆暑期中央松數營報名表</h1>
 	<form class='form-horizontal' role='form' id='joinForm' action='processJoin.php' method='POST'>
 		<p align='center'>放輕鬆，報名松數營很簡單！</p>
 <?php
@@ -27,9 +28,11 @@
 		// 顯示報名成功頁面
 	}else{
 		// 顯示報名頁面
+		
+		// 報名表陣列
 		$idInfo = array(
-			0=>array("id"=>"name", "labelName"=>"姓名", "value"=>$_SESSION['name'], "additionClass"=>"", "placeholder"=>"姓名"),
-			1=>array("id"=>"nickname", "labelName"=>"綽號", "value"=>$_SESSION['nickname'], "additionClass"=>"", "placeholder"=>"綽號"),
+			0=>array("id"=>"name", "labelName"=>"姓名", "value"=>$_SESSION['name'], "additionClass"=>$_SESSION['additionClass']['name'], "placeholder"=>"姓名"),
+			1=>array("id"=>"nickname", "labelName"=>"綽號", "value"=>$_SESSION['nickname'], "additionClass"=>$_SESSION['additionClass']['nickname'], "placeholder"=>"綽號"),
 		);
 		// printTextForm($id, $labelName, $value, $additionClass, $placeholder)
 		for ($i = 0; $i < sizeof($idInfo); $i++) {
@@ -37,6 +40,9 @@
 		}
 	}
 ?>
+		<div align='right'>
+			<input type='submit' class='btn btn-default submit' value='完成'>
+		</div>
 	</form>
 </div>
 </body
