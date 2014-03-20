f44cbf1a0bad49e42baa236950683759385ce4a3
LarryStanley <Stanley@mcl46.mcl.math.ncu.edu.tw>
2014-03-20 20:06:22 +0800

    Add require section.

--- join/processJoin.php
+++ join/processJoin.php
@@ -4,11 +4,13 @@
 	// 檢查欄位是否正確
 	$checkColumn = true;
 	for ($i = 0; $i < sizeof($id); $i++){
-		if (empty($_POST[$id[$i]['id']])){
-			$_SESSION['additionClass'][$id[$i]['id']] = "has-error";
-			$checkColumn = false;
-		}else
-			$_SESSION['additionClass'][$id[$i]['id']] = "has-success";
+		if ($id[$i]['required']){
+			if (empty($_POST[$id[$i]['id']])){
+				$_SESSION['additionClass'][$id[$i]['id']] = "has-error";
+				$checkColumn = false;
+			}else
+				$_SESSION['additionClass'][$id[$i]['id']] = "has-success";
+		}
 	}
 	
 	// 儲存資料
