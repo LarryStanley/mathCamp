f76a15685ee3674006d139514dd8cc9dcc341b7e
LarryStanley <Stanley@mcl46.mcl.math.ncu.edu.tw>
2014-03-20 16:59:13 +0800

    Add success.php.

---
+++ join/success.php
@@ -0,0 +1,25 @@
+<?php
+	session_start();
+?>
+<html>
+<head>
+<!-- Default setting -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+<!-- bootstrap -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+<!-- css animate  -->
+<link rel="stylesheet" href="../animate.css">
+
+<!-- self css -->
+<link rel="stylesheet" href="../style.css">
+<script>
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+</head>
+<body>
+<?php
+	session_destroy();
+?>
+</body>
+</html>
