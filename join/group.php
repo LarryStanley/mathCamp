00625b139cf4b112d7094cdaae66f6ff8464609b
LarryStanley <Stanley@mcl46.mcl.math.ncu.edu.tw>
2014-03-21 01:36:30 +0800

    Add group name.

--- join/group.php
+++ join/group.php
@@ -25,12 +25,17 @@
 	<h1 align='center'>第15屆暑期中央松數營報名表</h1>
 	<form class='form-horizontal' role='form' id='joinForm' action='processGroup.php' method='POST'>
 	<?php
+		$chineseNumber = array('ㄧ', '二', '三', '四', '五');
 		if (!$_SESSION['placeholder']){
 			$_SESSION['placeholder']['groupSchool'] = '學校';
 			$_SESSION['placeholder']['groupCity'] = '縣/市';
+			$_SESSION['placeholder']['groupName'.$i] = '姓名'.$chineseNumber[$i];
 		}
 		echo printTextForm('school', '學校',  $_SESSION['data']['groupSchool'], $_SESSION['additionClass']['groupSchool'], $_SESSION['placeholder']['groupSchool'], '');
 		echo printTextForm('city', '縣/市',  $_SESSION['additionClass']['groupCity'], $_SESSION['additionClass']['groupCity'], $_SESSION['placeholder']['groupCity'], '');
+		echo '<HR>';
+		for ($i= 0; $i < 5; $i++)
+			echo printTextForm('groupName'.$i, '姓名'.$chineseNumber[$i],  $_SESSION['data']['groupName'.$i], $_SESSION['additionClass']['groupName'.$i], $_SESSION['placeholder']['groupName'.$i], '');
 	?>
 		<div align='right'>
 			<input type='submit' class='btn btn-default submit' value='完成'>
--- join/index.php
+++ join/index.php
@@ -26,7 +26,7 @@
 	<form class='form-horizontal' role='form' id='joinForm' action='processJoin.php' method='POST'>
 		<p align='center'>放輕鬆，報名松數營很簡單！ 我們需了解你的一些基本資料</p>
 <?php
-	if ($_SESSION['placeholder'] == ''){
+	if (!$_SESSION['placeholder'] == ''){
 		for ($i = 0; $i < sizeof($id); $i++) {
 			if ($id[$i]['type'] == 'text'){
 				$_SESSION['placeholder'][$id[$i]['id']] = $id[$i]['name'];
