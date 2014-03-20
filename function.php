26c38170f435cbdd682103df4199786cc77f2940
LarryStanley <Stanley@mcl46.mcl.math.ncu.edu.tw>
2014-03-20 15:57:30 +0800

    Add the check column function.

--- function.php
+++ function.php
@@ -1,6 +1,6 @@
 <?php
 	function printTextForm($id, $labelName, $value, $additionClass, $placeholder) {
-		$result = "<div class='form-group".$additionClass."' id='".$id."Form'>";
+		$result = "<div class='form-group ".$additionClass."' id='".$id."Form'>";
 		$result = $result."<label class='col-sm-4 control-label'>".$labelName."</label>";
 		$result = $result."<div class='col-sm-8'><input type='text' class='form-control' id='".$id."' name='".$id."' placeholder='".$placeholder."' value='".$value."'>";
 		$result = $result."</div></div>";
--- join/processJoin.php
+++ join/processJoin.php
@@ -1,4 +1,24 @@
 <?php
+	session_start();
+	$id = array(
+		0=> 'name',
+		1=> 'nickname',
+	);
 	// 檢查欄位是否正確
-	echo $_POST['name'];
+	$checkColumn = true;
+	for ($i = 0; $i < sizeof($id); $i++){
+		if (empty($_POST[$id[$i]])){
+			$_SESSION['additionClass'][$id[$i]] = "has-error";
+			$checkColumn = false;
+		}else
+			$_SESSION['additionClass'][$id[$i]] = "has-success";
+	}
+	
+	// 若欄位不正確返回 index.php
+	// 若欄位正確前往   result.php
+	if (!$checkColumn)
+		head('index.php');
+	else
+		echo '成功';
+	
 ?>
