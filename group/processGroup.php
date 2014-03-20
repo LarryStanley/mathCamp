147b4954cd25e6e6fe2461e70a254f633bb0918f
LarryStanley <Stanley@mcl46.mcl.math.ncu.edu.tw>
2014-03-21 02:42:24 +0800

    Test the bug.

--- group/processGroup.php
+++ group/processGroup.php
@@ -14,10 +14,10 @@
 	$_SESSION['data']['groupSchool'] = $_POST['school'];
 	$_SESSION['data']['groupCity'] = $_POST['city'];
 	
-	if ($count != '3' || $count != '5'){
+	/*if ($count ==){
 		$checkColumn = false;
 		$_SESSION['groupAlert'] = '團報人數必須三人或五人喔！！'.$count.$checkColumn;
-	}
+	}*/
 	
 	// 若欄位不正確返回 index.php
 	// 若欄位正確前往   result.php
